19f0929faf376d89c4a38ff6a35e88545a340705
Gar2d2 <pghost286@gmail.com>
2020-11-02 21:20:21 +0100

    xd

---
+++ TamSkadLecaTracery/Source/TamSkadLecaTracery/DefaultPlayerController.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DefaultPlayerController.h"
+void ADefaultPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADefaultPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}

d33a3a7471f9dcdf7fca443e56e93661a9fc258c
Gar2d2 <pghost286@gmail.com>
2021-04-02 12:13:27 +0200

    backup

---
+++ TamSkadLecaTracery/Source/TamSkadLecaTracery/MultiplayerWidgetParent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MultiplayerWidgetParent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAMSKADLECATRACERY_API UMultiplayerWidgetParent : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

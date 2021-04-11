b374aaddcb8ec6de9016517d35e2618dfed5c1a8
Gar2d2 <pghost286@gmail.com>
2021-04-11 11:28:36 +0200

    repaired update HP

---
+++ TamSkadLecaTracery/Source/TamSkadLecaTracery/TeamDeathmatch.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TamSkadLecaTraceryGameModeBase.h"
+#include "TeamDeathmatch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TAMSKADLECATRACERY_API ATeamDeathmatch : public ATamSkadLecaTraceryGameModeBase
+{
+	GENERATED_BODY()
+	
+};
